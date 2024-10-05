9676d36bf661a2f708093ee29af22bfb228b6be1
Almaz <almaz.kzn2005@yandex.ru>
2024-10-05 22:59:10 +0300

    feat: add Type enum

---
+++ src/main/java/backend/academy/Type.java
@@ -0,0 +1,15 @@
+package backend.academy;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Type {
+    NORMAL(1), SAND(5), ICE(0);
+    private final int type;
+
+    @Override public String toString() {
+        return String.valueOf(type);
+    }
+}
