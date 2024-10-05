e9fad7d987be348d83ab1ba7e3ff99f1b7c4ba99
Almaz <almaz.kzn2005@yandex.ru>
2024-10-05 22:14:16 +0300

    feat: add Maze class

---
+++ src/main/java/backend/academy/Maze.java
@@ -0,0 +1,10 @@
+package backend.academy;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public final class Maze {
+    private final int height;
+    private final int width;
+    private final Cell[][] grid;
+}
