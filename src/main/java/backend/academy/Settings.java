19d09497695735e4b11c72aff267f7a5c806e35b
Almaz <almaz.kzn2005@yandex.ru>
2024-10-05 23:06:34 +0300

    feat: add Settings class

---
+++ src/main/java/backend/academy/Settings.java
@@ -0,0 +1,9 @@
+package backend.academy;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Settings {
+    public static final int MAX_HEIGHT = 30;
+    public static final int MAX_WIDTH = 30;
+}
