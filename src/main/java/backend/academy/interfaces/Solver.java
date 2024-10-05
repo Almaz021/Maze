47d99ff317baaf3f13888829ac316ce0481bb06b
Almaz <almaz.kzn2005@yandex.ru>
2024-10-05 23:05:24 +0300

    feat: add Solver interface

---
+++ src/main/java/backend/academy/interfaces/Solver.java
@@ -0,0 +1,9 @@
+package backend.academy.interfaces;
+
+import backend.academy.Coordinate;
+import backend.academy.Maze;
+import java.util.List;
+
+public interface Solver {
+    List<Coordinate> solve(Maze maze, Coordinate start, Coordinate end);
+}
