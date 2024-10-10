ae719bf6368831d3df33d04f65824dd3304f8dfe
Almaz <almaz.kzn2005@yandex.ru>
2024-10-10 17:40:02 +0300

    refactor: change Maze from class to record

--- src/main/java/backend/academy/Maze.java
+++ src/main/java/backend/academy/Maze.java
@@ -1,11 +1,6 @@
 package backend.academy;
 
 import backend.academy.entities.Cell;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
-public final class Maze {
-    private final int height;
-    private final int width;
-    private final Cell[][] grid;
+public record Maze(int height, int width, Cell[][] grid) {
 }
