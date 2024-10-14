9772d510c180244250d684c4438cfb1c6ef7a67d
Almaz <almaz.kzn2005@yandex.ru>
2024-10-14 20:33:30 +0300

    feat: add Direction enum

---
+++ src/main/java/backend/academy/enums/Direction.java
@@ -0,0 +1,10 @@
+package backend.academy.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Direction {
+    UP, RIGHT, DOWN, LEFT
+}
