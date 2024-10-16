275310115f0fc738a0a0d110274ead0d73120c4a
Almaz <almaz.kzn2005@yandex.ru>
2024-10-16 22:53:15 +0300

    feat: add start logic to Main class

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,19 @@
 package backend.academy;
 
+import backend.academy.services.ExceptionHandlerService;
+import backend.academy.services.StartService;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
+        try {
+            StartService startService = new StartService();
+            startService.start();
+
+        } catch (Exception e) {
+            ExceptionHandlerService.handleException(e);
+            System.exit(1);
+        }
     }
 }
