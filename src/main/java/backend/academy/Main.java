1ffbda1ca30e851d1802d686733a1e9a96dcc87f
Almaz <almaz.kzn2005@yandex.ru>
2024-10-17 17:39:31 +0300

    refactor: update start logic

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,14 +1,24 @@
 package backend.academy;
 
+import backend.academy.renderer.BaseRenderer;
 import backend.academy.services.ExceptionHandlerService;
 import backend.academy.services.StartService;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
         try {
-            StartService startService = new StartService();
+            StartService startService = new StartService(
+                new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8)),
+                new BaseRenderer(),
+                new SecureRandom(),
+                new MainInterface(new PrintWriter(System.out, true, StandardCharsets.UTF_8)));
             startService.start();
 
         } catch (Exception e) {
