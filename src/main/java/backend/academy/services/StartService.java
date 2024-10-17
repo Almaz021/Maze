922602d407eac1be328f008db7b02fe2915912ff
Almaz <almaz.kzn2005@yandex.ru>
2024-10-17 17:39:57 +0300

    feat: add StartService class

---
+++ src/main/java/backend/academy/services/StartService.java
@@ -0,0 +1,158 @@
+package backend.academy.services;
+
+import backend.academy.MainInterface;
+import backend.academy.entities.Coordinate;
+import backend.academy.entities.Maze;
+import backend.academy.enums.Type;
+import backend.academy.factories.GeneratorFactory;
+import backend.academy.factories.SolverFactory;
+import backend.academy.interfaces.Generator;
+import backend.academy.interfaces.Solver;
+import backend.academy.renderer.BaseRenderer;
+import backend.academy.settings.Settings;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.security.SecureRandom;
+import java.util.List;
+import java.util.stream.IntStream;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2 @RequiredArgsConstructor
+public class StartService {
+    private final BufferedReader reader;
+    private final BaseRenderer renderer;
+    private final SecureRandom random;
+    private final MainInterface mainInterface;
+    private Maze maze;
+
+    public void start() throws IOException {
+        Generator generator;
+        Solver solver;
+        mainInterface.helloMessage();
+
+        mainInterface.chooseWidth();
+        String w = getInput();
+        int width = checkSize(w, Settings.MIN_WIDTH, Settings.MAX_WIDTH);
+
+        mainInterface.chooseHeight();
+        String h = getInput();
+        int height = checkSize(h, Settings.MIN_HEIGHT, Settings.MAX_HEIGHT);
+
+        mainInterface.chosenSize(height, width);
+
+        mainInterface.chooseGenerator();
+        String g = getInput();
+        generator = selectGenerator(g);
+        mainInterface.chosenGenerator(generator);
+
+        mainInterface.chooseSolver();
+        String s = getInput();
+        solver = selectSolver(s);
+        mainInterface.chosenSolver(solver);
+
+        mainInterface.generateMaze();
+
+        // Maze generation
+        maze = generator.generate(height, width);
+        StringBuilder mazeString = renderer.render(maze);
+        mainInterface.showMaze(mazeString);
+
+        String coords;
+        int firstPointX;
+        int firstPointY;
+        int secondPointX;
+        int secondPointY;
+
+        mainInterface.choosePoint();
+        mainInterface.choosePointCoordinates();
+        do {
+            coords = getInput();
+        } while (checkCoordinates(coords, height, width));
+
+        firstPointX = Integer.parseInt(coords.split(" ")[0]);
+        firstPointY = Integer.parseInt(coords.split(" ")[1]);
+        Coordinate firstPoint = new Coordinate(firstPointY, firstPointX);
+        mainInterface.chosenPoint(firstPointX, firstPointY);
+
+        mainInterface.choosePointCoordinates();
+        do {
+            coords = getInput();
+        } while (checkCoordinates(coords, height, width));
+
+        secondPointX = Integer.parseInt(coords.split(" ")[0]);
+        secondPointY = Integer.parseInt(coords.split(" ")[1]);
+        Coordinate secondPoint = new Coordinate(secondPointY, secondPointX);
+        mainInterface.chosenPoint(secondPointX, secondPointY);
+
+        mainInterface.calculatePath();
+
+        List<Coordinate> path;
+        path = solver.solve(maze, firstPoint, secondPoint);
+        mazeString = renderer.render(maze, path);
+        mainInterface.showMaze(mazeString);
+
+    }
+
+    public String getInput() throws IOException {
+        String input = reader.readLine();
+        return input == null ? "" : input;
+    }
+
+    public int checkSize(String size, int min, int max) {
+        try {
+            int s = Integer.parseInt(size);
+            if (s >= min && s <= max && s % 2 != 0) {
+                return s;
+            } else {
+                mainInterface.chooseRandom();
+                return selectRandomSize(min, max);
+            }
+        } catch (NumberFormatException e) {
+            mainInterface.chooseRandom();
+            return selectRandomSize(min, max);
+        }
+    }
+
+    public boolean checkCoordinates(String coordinates, int height, int width) {
+        try {
+            String[] parts = coordinates.split(" ");
+            int x = Integer.parseInt(parts[0]);
+            int y = Integer.parseInt(parts[1]);
+
+            boolean inBounds = (0 <= x && x <= width && 0 <= y && y < height);
+
+            if (inBounds) {
+                if (checkPassage(y, x)) {
+                    return false;
+                } else {
+                    mainInterface.wrongPoint();
+                }
+            } else {
+                mainInterface.incorrectCoordinates();
+            }
+
+            return true;
+        } catch (NumberFormatException e) {
+            mainInterface.onlyNumbers();
+            return true;
+        }
+    }
+
+    public boolean checkPassage(int y, int x) {
+        return List.of(Type.NORMAL, Type.ICE, Type.SAND).contains(maze.grid()[y][x].type());
+    }
+
+    public int selectRandomSize(int min, int max) {
+        int[] range = IntStream.iterate(min, n -> n + 2).limit(max / 2).toArray();
+        return range[random.nextInt(range.length)];
+    }
+
+    public Generator selectGenerator(String generator) {
+        return new GeneratorFactory(random).selectGenerator(generator);
+    }
+
+    public Solver selectSolver(String solver) {
+        return new SolverFactory(random).selectSolver(solver);
+    }
+}
