31d61c861e509fcb6bc58372d620c7b229703468
Almaz <almaz.kzn2005@yandex.ru>
2024-10-27 17:36:11 +0300

    refactor: split large start method into smaller helper methods

--- src/main/java/backend/academy/services/StartService.java
+++ src/main/java/backend/academy/services/StartService.java
@@ -30,83 +30,103 @@ public class StartService {
     public void start() throws IOException {
         mainInterface.helloMessage();
 
-        mainInterface.chooseWidth();
-        String w = getInput();
-        int width = checkSize(w, Settings.MIN_WIDTH, Settings.MAX_WIDTH);
+        int width = getMazeWidth();
 
-        mainInterface.chooseHeight();
-        String h = getInput();
-        int height = checkSize(h, Settings.MIN_HEIGHT, Settings.MAX_HEIGHT);
+        int height = getMazeHeight();
 
         mainInterface.chosenSize(height, width);
 
+        Generator generator = getMazeGenerator();
+
+        Solver solver = getMazeSolver();
+
+        generateMaze(generator, height, width);
+
+        if (isModificationRequested()) {
+            modifyMaze(height, width);
+        }
+
+        Coordinate firstPoint = getPointCoordinates(height, width);
+        Coordinate secondPoint = getPointCoordinates(height, width);
+
+        calculateAndShowPath(solver, firstPoint, secondPoint);
+
+        mainInterface.finish();
+
+    }
+
+    public int getMazeWidth() throws IOException {
+        mainInterface.chooseWidth();
+        String w = getInput();
+        return checkSize(w, Settings.MIN_WIDTH, Settings.MAX_WIDTH);
+    }
+
+    public int getMazeHeight() throws IOException {
+        mainInterface.chooseHeight();
+        String h = getInput();
+        return checkSize(h, Settings.MIN_HEIGHT, Settings.MAX_HEIGHT);
+    }
+
+    public Generator getMazeGenerator() throws IOException {
         mainInterface.chooseGenerator();
         String g = getInput();
         Generator generator = selectGenerator(g);
         mainInterface.chosenGenerator(generator);
+        return generator;
+    }
 
+    public Solver getMazeSolver() throws IOException {
         mainInterface.chooseSolver();
         String s = getInput();
         Solver solver = selectSolver(s);
         mainInterface.chosenSolver(solver);
+        return solver;
+    }
 
+    public void generateMaze(Generator generator, int height, int width) {
         mainInterface.generateMaze();
-
         maze = generator.generate(height, width);
         StringBuilder mazeString = renderer.render(maze);
         mainInterface.showMaze(mazeString);
+    }
 
+    public boolean isModificationRequested() throws IOException {
         mainInterface.requestForModification();
         String answer = getInput();
-        if ("YES".equalsIgnoreCase(answer)) {
-            mainInterface.chooseModifier();
-            String m = getInput();
-            Modifier modifier = selectModifier(m);
-            mainInterface.chosenModifier(modifier);
+        return "YES".equalsIgnoreCase(answer);
+    }
 
-            mainInterface.modifyMaze();
-            maze = modifier.modify(height, width);
+    public void modifyMaze(int height, int width) throws IOException {
+        mainInterface.chooseModifier();
+        String m = getInput();
+        Modifier modifier = selectModifier(m);
+        mainInterface.chosenModifier(modifier);
+        mainInterface.modifyMaze();
+        maze = modifier.modify(height, width);
+        StringBuilder mazeString = renderer.render(maze);
+        mainInterface.showMaze(mazeString);
+    }
 
-            mazeString = renderer.render(maze);
-            mainInterface.showMaze(mazeString);
-        }
-
-        String coords;
-        int firstPointX;
-        int firstPointY;
-        int secondPointX;
-        int secondPointY;
-
+    public Coordinate getPointCoordinates(int height, int width) throws IOException {
         mainInterface.choosePoint();
         mainInterface.choosePointCoordinates();
+        String coords;
         do {
             coords = getInput();
         } while (checkCoordinates(coords, height, width));
 
-        firstPointX = Integer.parseInt(coords.split(" ")[0]);
-        firstPointY = Integer.parseInt(coords.split(" ")[1]);
-        Coordinate firstPoint = new Coordinate(firstPointY, firstPointX);
+        int firstPointX = Integer.parseInt(coords.split(" ")[0]);
+        int firstPointY = Integer.parseInt(coords.split(" ")[1]);
         mainInterface.chosenPoint(firstPointX, firstPointY);
+        return new Coordinate(firstPointY, firstPointX);
+    }
 
-        mainInterface.choosePointCoordinates();
-        do {
-            coords = getInput();
-        } while (checkCoordinates(coords, height, width));
-
-        secondPointX = Integer.parseInt(coords.split(" ")[0]);
-        secondPointY = Integer.parseInt(coords.split(" ")[1]);
-        Coordinate secondPoint = new Coordinate(secondPointY, secondPointX);
-        mainInterface.chosenPoint(secondPointX, secondPointY);
-
+    public void calculateAndShowPath(Solver solver, Coordinate firstPoint, Coordinate secondPoint) {
         mainInterface.calculatePath();
-
         List<Coordinate> path;
         path = solver.solve(maze, firstPoint, secondPoint);
-        mazeString = renderer.render(maze, path);
+        StringBuilder mazeString = renderer.render(maze, path);
         mainInterface.showMaze(mazeString);
-
-        mainInterface.finish();
-
     }
 
     public String getInput() throws IOException {
